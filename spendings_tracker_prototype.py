2081114abffabce4e69396d96ac11448f7f765dd
Charlie <35852547+charlies12@users.noreply.github.com>
2023-08-04 11:50:36 -0700

    Add files via upload

--- spendings_tracker_prototype.py
+++ spendings_tracker_prototype.py
@@ -8,26 +8,30 @@ def item_name():
     name_list = []
     begin = True
     while begin:
-        name = input("Enter item name: \nEnter q to exit.\n")
-        name_list.append(name)
-        if name.lower() == "q":
-            name_list.pop()
-            begin = False
-            return name_list
+        name = input("Enter item name: \nEnter q to end entry.\n")
+        if name.strip() == "":
+            print("No name entered. Enter an item name.")
+        else:
+            name_list.append(name)
+            if name.lower() == "q":
+                name_list.pop()
+                begin = False
+                return name_list
 
 
 # item_price returns price of item as a float
 def item_price():
+
     price_list = []
     begin = True
     while begin:
         try:
-            price = float(input("Enter item price: \n" + "Enter -1 to exit.\n"))
-            price_list.append(price)
-            if price < 0:
-                price_list.pop()
-                begin = False
-                return price_list
+            for name in result_item_name:
+                price = float(input(f"Enter {name} price: \n"))
+                price_list.append(price)
+                if name == result_item_name[-1]:
+                    begin = False
+                    return price_list
         except ValueError:
             print("Enter only numbers and a decimal point if needed.")
 
@@ -36,11 +40,8 @@ def item_price():
 def name_and_price(name_value, price_value):
     result_item_name = name_value
     result_item_price = price_value
-    # name_value = item_name()
-    # price_value = item_price()
 
     name_and_price_pairs = dict(zip(result_item_name, result_item_price))
-    # name_and_price_pairs = dict(zip(name_value, price_value))
     return name_and_price_pairs
 
 
@@ -55,7 +56,7 @@ def total_cost():
 
 # writes name, price, and total to a file using json import
 def copy_to_file():
-    with open("purchases_tracker", "w") as file:
+    with open("purchases_tracker", "a+") as file:
         file.write(json.dumps(name_and_price(result_item_name, result_item_price)) + "\n")
         file.write(json.dumps(total_cost()) + "\n")
 
